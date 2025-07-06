f30c76dc65d46259fddff527c9bbc58d54607c7d
Bryant <dumasb@umich.edu>
2025-07-06 17:50:57 -0400

    First working draft

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_cors import CORS
+import requests
+import json
+
+app = Flask(__name__)
+CORS(app, origins=["http://localhost:5000"]) 
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    input = request.form.get('input')
+    data = {"Form-Input":input}
+    post_request = requests.post("https://jfmyiqt6rb.execute-api.us-east-1.amazonaws.com/Add-Task", json=data)
+    return redirect(url_for('index')) 
+
+@app.route('/view-task', methods=['GET'])
+def view_task():
+    get_request = requests.get("https://jfmyiqt6rb.execute-api.us-east-1.amazonaws.com/Get-Task")
+    Json_obj = json.loads(get_request.text)
+    print(type(Json_obj))
+    FIlter_task_dictionary =  [dictionary for dictionary in Json_obj if dictionary['task_description'] != '']
+    tasks =  [dictionary['task_description'] for dictionary in FIlter_task_dictionary]
+    # Example structure: {"tasks": ["Buy milk", "Call mom"]}
+    
+    return render_template("view-task.html", tasks=tasks)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ static/main.js
@@ -0,0 +1,4 @@
+
+
+const view_task_button = document.getElementById('view_task');
+console.log(view_task_button)
